ab6568a622c1e0b9d66d44fecc3a118b38c3b6c2
Ranniex <deporsam@hotmail.com>
2020-12-15 00:03:20 -0500

    cors error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://chatvid-backend.herokuapp.com/",
       methods: ["GET", "POST"]
     }
   });
